6d00c0020a27b01402f09aa6982ff3329a5d83a8
Ramanqul <buzaubakov.raman@gmail.com>
2018-07-26 23:55:44 +0600

    Initial commit

---
+++ src/main/java/com/upwork/ramanqul/batik/extension/text2/test/Text2ExtensionRenderTest.java
@@ -0,0 +1,20 @@
+package com.upwork.ramanqul.batik.extension.text2.test;
+
+import org.apache.batik.apps.rasterizer.Main;
+
+public class Text2ExtensionRenderTest {
+
+    
+    public static void main(String[] args) {
+//        System.setProperty("java.security.policy","my.policy");
+//        System.setSecurityManager(new SecurityManager());
+        
+//        String file = "D:\\DEV\\work\\out\\DavidGoldfarb\\custom_batik\\batik\\samples\\extensions\\star.svg";
+//        String file = "D:\\DEV\\work\\out\\DavidGoldfarb\\custom_batik\\batik\\samples\\extensions\\custText.svg";
+//        String file = "D:\\DEV\\work\\out\\DavidGoldfarb\\custom_batik\\batik\\batik-svgrasterizer\\test\\custText.svg";
+        
+//        String options[] = new String[] {"-d", "output", "-m", "image/png", file};
+        Main.main(args);
+    }
+    
+}
